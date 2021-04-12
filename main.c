ebddf0be0310916b5b36370078928d71acd8995c
Vinícius Almeida <>
2021-04-12 12:58:28 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  float _a=0.4;
+  float _b=0.6;
+  float _x=2.0;//Alterar por qualquer valor entre 0 e 10
+  float _y=3.0;//Alterar por qualquer valor entre 0 e 10
+  printf("%e\n",_a*_x+_b*_y);
+  return 0;
+}
